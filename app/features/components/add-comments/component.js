c4dca0c7769d2f4ed8066620d34b5f7e522c19cc
Erik Hanchett <icystorm@gmail.com>
2016-01-03 17:02:49 -0500

    fixed alert

--- app/features/components/add-comments/component.js
+++ app/features/components/add-comments/component.js
@@ -3,8 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	actions:{
 		submitComment(author, body){
-			alert(author);
-			alert(body);
 			let post = this.get('post');
 			this.sendAction('store',author,body,post);
 			this.setProperties({
